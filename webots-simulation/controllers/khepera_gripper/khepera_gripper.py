a9337363ee3092d565d2139e6ea2d3e119926407
Angel Sylvester <sylve057@umn.edu>
2022-07-18 14:23:32 -0500

    initial env set-up

---
+++ webots-simulation/controllers/khepera_gripper/khepera_gripper.py
@@ -0,0 +1,96 @@
+"""khepera_gripper controller."""
+
+# You may need to import some classes of the controller module. Ex:
+#  from controller import Robot, Motor, DistanceSensor
+from controller import Robot, Motor, DistanceSensor, Camera, CameraRecognitionObject 
+
+# create the Robot instance.
+robot = Robot()
+
+# get the time step of the current world.
+timestep = int(robot.getBasicTimeStep())
+open_grip = 0.029
+closed_grip = 0.005
+
+motor = robot.getDevice('motor')
+
+leftMotor = robot.getDevice('left wheel motor')
+rightMotor = robot.getDevice('right wheel motor')
+
+leftMotor.setVelocity(0)
+rightMotor.setVelocity(0)
+
+leftGrip = robot.getDevice('left grip')
+rightGrip = robot.getDevice('right grip')
+
+ds = robot.getDevice('ds')
+ds.enable(timestep)
+
+
+# You should insert a getDevice-like function in order to get the
+# instance of a device of the robot. Something like:
+#  motor = robot.getDevice('motorname')
+#  ds = robot.getDevice('dsname')
+#  ds.enable(timestep)
+
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
+i = 0 
+
+while robot.step(timestep) != -1:
+    if (i == 0):
+        # opens the gripper 
+        leftGrip.setPosition(open_grip)
+        rightGrip.setPosition(open_grip)
+        
+    elif (i == 20):
+        motor.setPosition(0) # arm down 
+    
+    elif (i == 40):
+        # closes the gripper 
+        leftGrip.setPosition(closed_grip)
+        rightGrip.setPosition(closed_grip) 
+    
+    elif (i == 80):
+        motor.setPosition(-1.4) # arm up
+        
+    elif (i == 100):
+        leftMotor.setPosition(float('inf'))
+        leftMotor.setVelocity(-2) # turns robot while backing
+        rightMotor.setPosition(float('inf'))
+        rightMotor.setVelocity(2)
+        
+    elif (i == 140):
+        leftMotor.setPosition(float('inf'))
+        leftMotor.setVelocity(10) # moves robot forward
+        rightMotor.setPosition(float('inf'))
+        rightMotor.setVelocity(10)
+ 
+        
+    # Read the sensors:
+    # Enter here functions to read sensor data, like:
+    
+    # read distance sensor value 
+    dist_val = ds.getValue()
+    
+    # camera info 
+    firstObject = Camera.getRecognitionObjects()[0]
+    id = firstObject.get_id()
+    position = firstObject.get_position()
+    
+    
+    # detect obstacle 
+    
+    # detect other robot 
+    
+
+    # Process sensor data here.
+
+    # Enter here functions to send actuator commands, like:
+    #  motor.setPosition(10.0)
+    
+    i+=1
+    
+    pass
+
+# Enter here exit cleanup code.
---
+++ webots-simulation/controllers/supervisor_controller/supervisor_controller.py
@@ -0,0 +1,32 @@
+from controller import Supervisor
+
+TIME_STEP = 32
+
+robot = Supervisor()  # create Supervisor instance
+
+# [CODE PLACEHOLDER 1]
+
+# i = 0
+# while robot.step(TIME_STEP) != -1:
+  # bb8_node = robot.getFromDef('e-puck')
+  # translation_field = bb8_node.getField('translation')
+  # if i == 0: 
+      # new_value = [2.5, 0, 0]
+      # translation_field.setSFVec3f(new_value)
+
+  # i += 1
+  
+ 
+# def main():
+    # for a in range(0.5, 10, 0.1):
+        # for b in range(0.1, 5, 0.5):
+             # translation_field.setSFVec3f([0,0,0])
+             # move robot to initial position 
+             
+             # for i in range(0, 30, TIME_STEP/1000):
+                 # actuate motors (probably in epuck file)
+                 # print('testing time-steps', i) 
+             
+             # compute fitness
+     
+ 
