9bf8101d8caac9b51c8b51a67ff9cb1180cf9e7a
Angel Sylvester <sylve057@umn.edu>
2022-07-25 16:31:46 -0500

    mutation (untested) script set up

--- webots-simulation/controllers/ga_supervisor/robot_pop.py
+++ webots-simulation/controllers/ga_supervisor/robot_pop.py
@@ -2,28 +2,85 @@
 
 import random 
 import pandas as pd 
+import numpy as np 
 
-def create_random_population(size, gene_size): 
+
+
+def create_random_population(size, gene_list): 
     # will control threshold of detection & speed 
     # rule-set generated for each robot 
     
-    ruleset = []
+    ruleset = ""
+    pop = ""
     for pop in range(size): 
-        for gene in range(gene_size):
-            pass
-        
-    return str(random.randint(3, 6)) + " " + str(random.randint(3, 6)) + " " + str(random.randint(3, 6)) 
+        for gene in gene_list:
+            length_gene = gene[-1]
+            ruleset += generate_random_act(length_gene) + "*"
+        pop += ruleset + " " 
     
-def generate_random_act():
-    pass
+    return pop         
+        
+    # return str(random.randint(3, 6)) + " " + str(random.randint(3, 6)) + " " + str(random.randint(3, 6)) 
+    
+def generate_random_act(length):
+    return np.random.randint(2, size = length)
+    
     
 def reproduce(r1, r2): 
     # if pass a certain threshold, will increment/decrement randomly by 
     # smaller amount 
     
     # if below, resample again 
+    new_genotype = []
     
-    pass
+    for i in range(len(r1)): # assuming this is a list of genotypes 
+        mom = r1[i]
+        dad = r2[i]
+        
+        child = crossover(mom, dad) 
+        child = mutate(child) 
+        new_genotype.append(child) 
     
+    return new_genotype 
+    
+def crossover(m, d): 
+    new_child = ""
+    
+    random_start = random.randint(len(m))
+    
+    new_child += m[:random_start] + d[random_start] 
+    
+    return new_child 
+
+def mutate(c, mut_prob): 
+
+    size = len(c) 
+    for i in range(size): 
+        if random.random() < mut_prob: 
+            if i == 0: 
+                # choose random position 
+                p = random.randint(len(c)/2)
+                # switch to different val 
+                if c[p] == 1:
+                    c[p] = 0
+                else: 
+                    c[p] = 1 
+            
+            
+            elif i == 1: 
+                # choose random position 
+                p = random.randint(len(c)/2, len(c))
+                # switch to different val 
+                if c[p] == 1:
+                    c[p] = 0
+                else: 
+                    c[p] = 1 
+            
+            else: 
+                pass 
+            
+    return c  
+    
+
     
 
