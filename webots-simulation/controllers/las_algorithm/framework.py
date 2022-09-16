9285998dfcbe7b453e7cefe47f15994be682edb7
Angel Sylvester <sylve057@umn.edu>
2022-09-16 12:50:50 -0500

    las algorithm framework fundamentals

--- webots-simulation/controllers/las_algorithm/framework.py
+++ webots-simulation/controllers/las_algorithm/framework.py
@@ -2,6 +2,7 @@
 # incorporates number of robots m
 
 import numpy as np
+import math 
 
 class LAS():
     def __init__(self, top_right = (-1, 0.8), bot_left = (0, 0), num_cells = 9, curr_pos = (0,0)): # assuming 3 rows, arbitrarily
@@ -34,6 +35,9 @@ class LAS():
                 top_rightx, top_righty = self.trx + (self.r_incre*j), self.tr_y - (self.c_incre*i)
                 bot_rightx, bot_righty = self.blx + (self.r_incre*j), self.bly - (self.c_incre*i)
                 self.cells.append((top_rightx, top_righty, bot_rightx, bot_righty))
+                
+        self.prob_vector = list(np.full((1, 3*num_per_row), (1/(3*num_per_row))))
+        self.M_vector = list(np.full(1, len(self.cells), 0))
 
         # precalculates neighbors
         for cell in self.cells:
@@ -51,6 +55,9 @@ class LAS():
                     if (abs(tpx - otpx) <= abs(self.r_incre) and abs(tpy - otpy) <= abs(self.c_incre)):
                         neigh.append(other_cell)
             self.neighbors[str(cell)] = neigh
+            
+        self.res_factor = 70 
+        self.delta_pen = 1 / ((len(self.cells)) * self.res_factor)
                         
 
     def update(self, original_tile, curr_pos):
@@ -81,3 +88,63 @@ class LAS():
                 # print('comparison satisfied') 
                 return self.cells.index(c)     
         return 'locate cell error'
+        
+    def reward(self, curr_tile):
+    
+        if curr_tile in self.M_vector:
+            return self.prob_vector
+        
+        for p in range(len(self.prob_vector)): 
+            self.M_vector.append(curr_tile)
+            sum = 1 
+            if p != curr_tile: 
+                new_p = p - self.delta_pen
+                if new_p < 0:
+                    new_p = 0
+                
+                self.prob_vector[p] = new_p
+                sum += new_p 
+                
+            else: 
+                elf.prob_vector[p] = 1 - sum 
+                
+        return self.prob_vector 
+        
+    def penalize(self, curr_tile): 
+        curr_min = self.prob_vector[0]
+        curr_max = self.prob_vector[0]
+    
+        for p in range(len(self.prob_vector)): 
+            sum = 1 
+            if p != curr_tile: 
+            
+                if self.prob_vector[p] > self.delta_pen * (len(self.cells) - 1)
+                    in_min = self.delta_pen * (len(self.cells) - 1
+                else: 
+                    in_min = self.prob_vector[p]
+                    
+                
+                if self.prob_vector[p] + (in_min / (len(self.cells) - 1)) > 1: 
+                    self.prob_vector[p] = self.prob_vector[p] + (in_min / (len(self.cells) - 1))
+                else: 
+                    self.prob_vector[p] = 1
+                    
+            else: 
+                if self.prob_vector[p] - (len(self.cells) - 1) * self.delta_pen > 0: 
+                    self.prob_vector[p] = self.prob_vector[p] - (len(self.cells) - 1) * self.delta_pen
+                else: 
+                    self.prob_vector[p] = 0
+                    
+                
+        return self.prob_vector 
+        
+     def re_direct(self, curr_tile): 
+         new_cell = random.choices(self.cells, self.prob_vector)
+         new_tile = self.cells.index(new_cell)
+         
+         tpx, tpy, brx, bry = self.cells[curr_tile]
+         otpx, otpy, obrx, obry = new_cell
+         
+         direction = math.atan((otpy - tpy), (otpx - tpx))
+         
+         return round(direction, 2)
