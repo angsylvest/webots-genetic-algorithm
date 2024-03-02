876076276e96b33226cc00c73e64ca2d3d26678b
Angel Sylvester <sylve057@umn.edu>
2024-03-02 11:40:03 -0600

    added more tests to assess env

--- webots-simulation/utils/shared_map.py
+++ webots-simulation/utils/shared_map.py
@@ -7,8 +7,6 @@ class CompleteMap():
         self.obstacle_size = obstacle_size
         self.agent_size = agent_size
 
-        
-
     def update_agent_positions(self, agent_positions):
         self.agent_positions = agent_positions
 
@@ -19,7 +17,9 @@ class CompleteMap():
         max_y = central_loc[1] + dim_size/2
 
         subset_obstacles = [(x, y) for (x, y) in self.obstacle_locations if min_x <= x <= max_x and min_y <= y <= max_y]
-        return subset_obstacles
+        subset_agents = [(x, y) for (x, y) in self.agent_positions if min_x <= x <= max_x and min_y <= y <= max_y]
+        
+        return subset_obstacles, subset_agents
     
     
 class LocalMap():
@@ -31,15 +31,42 @@ class LocalMap():
         self.local_dims = local_dim
 
     def identify_obstacle_distr_type(self):
-        # dense vs sparse based on # of obstacles (more than 50% vs less than 50% of open spaces)
-        # also, type: (organized, organized (unpredictable), corridor, open) 
+        total_area = self.local_dims ** 2
+        obstacle_area = len(self.obstacle_pos) * self.obstacle_size ** 2
+        open_space_area = total_area - obstacle_area
+        obstacle_density = obstacle_area / open_space_area
 
-        pass 
+        print(f'obstacle density: {obstacle_density}')
+
+        # if obstacle_density > 0.5:
+        #     return "dense"
+        # else:
+        #     return "sparse"
 
     def identify_agent_distr_type(self):
-        # dense vs sparse based on # of agents (more than 50% vs less than 50% of open spaces)
+        total_area = self.local_dims ** 2
+        agent_area = len(self.agent_pos) * self.agent_size ** 2
+        open_space_area = total_area - agent_area
+        agent_density = agent_area / open_space_area
 
-        pass 
+        print(f'agent density: {agent_density}')
+
+        # if agent_density > 0.5:
+        #     return "dense"
+        # else:
+        #     return "sparse"
+        
+
+    # def identify_obstacle_distr_type(self):
+    #     # dense vs sparse based on # of obstacles (more than 50% vs less than 50% of open spaces)
+    #     # also, type: (organized, organized (unpredictable), corridor, open) 
+
+    #     pass 
+
+    # def identify_agent_distr_type(self):
+    #     # dense vs sparse based on # of agents (more than 50% vs less than 50% of open spaces)
+
+    #     pass 
 
     def choose_coordination_type(self):
         # rule set: (righthand rule, queue-based (pause and go), go-away, follow)
@@ -55,4 +82,21 @@ class LocalMap():
         msg = ""
 
         return msg 
+    
+
+def __main__():
+    obstacle_locations = [(1, 1), (2, 2), (5, 5), (7, 7)]
+    complete_map = CompleteMap(obstacle_locations=obstacle_locations, dims=10, obstacle_size=1, agent_size=0.5)
+    complete_map.update_agent_positions([(3,3), (6,6)])
+    central_loc = (5, 5)
+    dim_size = 10
+    map_subset_obs, map_subset_ag = complete_map.subset(dim_size=dim_size, central_loc=central_loc)
+
+    local_map = LocalMap(obstacle_pos=map_subset_obs, obstacle_size=dim_size, agent_pos=map_subset_ag, agent_size= 0.5, local_dim=dim_size)
+    local_map.identify_agent_distr_type()
+    local_map.identify_obstacle_distr_type()
+
+if __name__ == "__main__":
+    __main__()
+
 
--- webots-simulation/utils/visualize_map.py
+++ webots-simulation/utils/visualize_map.py
@@ -2,20 +2,23 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 
 import numpy as np
-from shared_map import CompleteMap
+from shared_map import CompleteMap, LocalMap
 
-def visualize_subset(subset_obstacles, central_loc, dims, obstacle_locations, obstacle_size, agent_positions, agent_size):
+def visualize_subset(subset_obstacles, central_loc, dims, obstacle_size, agent_positions, agent_size, map_dims):
     fig, ax = plt.subplots()
-    ax.set_xlim(0, dims)
-    ax.set_ylim(0, dims)
+    
+    # Calculate the limits for the x and y axes based on the central location and subset dimension
+    min_x = max(0, central_loc[0] - dims/2)
+    max_x = min(central_loc[0] + dims/2, map_dims)  # Assuming 10 is the total dimension of the map
+    min_y = max(0, central_loc[1] - dims/2)
+    max_y = min(central_loc[1] + dims/2, map_dims)  # Assuming 10 is the total dimension of the map
+    
+    ax.set_xlim(min_x, max_x)
+    ax.set_ylim(min_y, max_y)
     
     # Plot central location
     ax.scatter(*central_loc, color='red', label='Central Location')
     
-    # Plot obstacles as squares
-    for (x, y) in obstacle_locations:
-        ax.add_patch(Rectangle((x - obstacle_size/2, y - obstacle_size/2), obstacle_size, obstacle_size, color='black'))
-
     # Plot subset obstacles
     for (x, y) in subset_obstacles:
         ax.add_patch(Rectangle((x - obstacle_size/2, y - obstacle_size/2), obstacle_size, obstacle_size, color='blue'))
@@ -31,16 +34,21 @@ def visualize_subset(subset_obstacles, central_loc, dims, obstacle_locations, ob
     ax.legend()
     plt.show()
 
+
 def __main__():
     obstacle_locations = [(1, 1), (2, 2), (5, 5), (7, 7)]
     complete_map = CompleteMap(obstacle_locations=obstacle_locations, dims=10, obstacle_size=1, agent_size=0.5)
     complete_map.update_agent_positions([(3,3), (6,6)])
     central_loc = (5, 5)
-    dim_size = 3
-    subset_obstacles = complete_map.subset(dim_size=dim_size, central_loc=central_loc)
+    dim_size = 10
+    map_subset_obs, map_subset_ag = complete_map.subset(dim_size=dim_size, central_loc=central_loc)
     
+    local_map = LocalMap(obstacle_pos=map_subset_obs, obstacle_size=dim_size, agent_pos=map_subset_ag, agent_size= 0.5, local_dim=dim_size)
+    local_map.identify_agent_distr_type()
+    local_map.identify_obstacle_distr_type()
+
     # Pass all required arguments to visualize_subset
-    visualize_subset(subset_obstacles, central_loc, 5, complete_map.obstacle_locations, complete_map.obstacle_size, complete_map.agent_positions, complete_map.agent_size)
+    visualize_subset(map_subset_obs, central_loc, 5, complete_map.obstacle_size, map_subset_ag, complete_map.agent_size, complete_map.dims)
 
 if __name__ == "__main__":
     __main__()
